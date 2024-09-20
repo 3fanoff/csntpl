a11a59d9bedcdb3c22847097cb0d25c54b58d30a
trifanov <trifanov@teamidea.ru>
2024-09-21 00:57:49 +0300

    footer layouting

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import aliasImporter from 'node-sass-alias-importer';
 import handlebars from 'vite-plugin-handlebars';
 
 console.log(process.env);
-const basePath =  process.env.GITHAB_PAGES_BASE_PATH ?? '/';
+const basePath =  process.env.GITHUB_PAGES_BASE_PATH ?? '/';
 
 export default {
   base: basePath,
@@ -24,6 +24,7 @@ export default {
           aliasImporter({
             "~bootstrap": resolve(__dirname, 'node_modules/bootstrap'),
             "~flag-icons": resolve(__dirname, 'node_modules/flag-icons'),
+            "@fonts": resolve(__dirname, 'src/fonts'),
           })
         ]
       }
@@ -32,6 +33,7 @@ export default {
   resolve: {
     alias: {
       '@scss': resolve(__dirname, 'src/scss'),
+      '@fonts': resolve(__dirname, 'src/fonts'),
     }
   },
   plugins: [
