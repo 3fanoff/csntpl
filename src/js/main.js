e0daac41e38d1ab1e2ec474cc46118ecf135e62a
trifanov <trifanov@teamidea.ru>
2024-09-22 19:49:55 +0300

    tabs layout & some footer fixes

--- src/js/main.js
+++ src/js/main.js
@@ -8,3 +8,35 @@ if ((carousels = document.querySelectorAll('.js-carousel-gallery')).length) {
     new CarouselGallery(carousel).init();
   });
 }
+
+const tabEmulationNodes = document.querySelectorAll('[data-emulation-tab]')
+Array.prototype.slice.call(tabEmulationNodes).forEach(emulationNode => {
+  emulationNode.addEventListener('click', (e) => {
+    const originalTabSelector = e.target.getAttribute('data-emulation-tab');
+    const originalTabNode = document.querySelector(originalTabSelector);
+    if (originalTabNode) originalTabNode.click();
+  })
+
+  /*emulationNode.addEventListener('show.bs.tab', function(event) {
+    console.log(event.target) // newly activated tab
+    console.log(bootstrap.Tab.getInstance(this)) // previous active tab
+  })*/
+});
+
+const tabOriginalNodes = document.querySelectorAll('[data-bs-toggle="tab"]');
+Array.prototype.slice.call(tabOriginalNodes).forEach(originalNode => {
+  originalNode.addEventListener('show.bs.tab', (e) => {
+    Array.prototype.slice.call(tabEmulationNodes).forEach(emulationNode => {
+      let currentTabSelector = emulationNode.getAttribute('data-emulation-tab');
+      if (currentTabSelector === '#' + e.target.id) {
+        emulationNode.classList.add('active');
+        return;
+      }
+      if (currentTabSelector === '#' + e.relatedTarget.id) {
+        emulationNode.classList.remove('active');
+      }
+    })
+    console.log('#' + e.target.id);
+    console.log('#' + e.relatedTarget.id);
+  })
+});
